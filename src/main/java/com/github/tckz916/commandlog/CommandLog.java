97a63fcd28268b1cfbcf96d8edfe7b74ea2abb60
tckz916 <tachikaze0916@gmail.com>
2015-11-30 00:48:40 +0900

    CommandLog

---
+++ src/main/java/com/github/tckz916/commandlog/CommandLog.java
@@ -0,0 +1,46 @@
+package com.github.tckz916.commandlog;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by tckz916 on 2015/09/27.
+ */
+public class CommandLog extends JavaPlugin implements Listener {
+
+    PluginManager pm = this.getServer().getPluginManager();
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+
+        pm.registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+
+        for (Player players : this.getServer().getOnlinePlayers()) {
+            if (players.hasPermission("commandlog.see")) {
+                players.sendMessage(ChatColor.GRAY + "[CL] " + player.getName() + ": " + event.getMessage());
+            }
+        }
+
+    }
+
+    private String coloring(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+}
